60c798648e55150f5e788774868d0cfc3cae90b4
Jeff Wheeler <jeffwheeler@gmail.com>
2014-02-20 22:08:47 -0800

    Initial import

---
+++ process.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+def threshold(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    thresh = 255-cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        thresholdType=cv2.THRESH_BINARY, blockSize=151, C=50)
+
+    k5 = np.ones((5,5),np.uint8)
+    k7 = np.ones((7,7),np.uint8)
+    g = cv2.erode(cv2.dilate(thresh, k7, iterations=1), k5, iterations=1)
+    # g = cv2.Canny(g, 50, 150, apertureSize=5)
+    return g
+
+def process(g, img):
+    gf = np.float32(g)
+    dst = cv2.cornerHarris(gf,7,21,0.04)
+    dst = cv2.dilate(dst,None)
+    g[dst>0.05*dst.max()]=0
+
+    # cont = cv2.
+    contours, hierarchy = cv2.findContours(g, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    for c in contours:
+        if cv2.contourArea(c) > 50:
+            ellipse = cv2.fitEllipse(c)
+            axes = ellipse[1]
+            ecc = max(axes)/min(axes)
+            print ecc
+            if ecc > 5:
+                cv2.drawContours(img, c, -1, (0, 0, 255), 1)
+            else:
+                cv2.drawContours(img, c, -1, (255, 0, 0), 1)
+        else:
+            cv2.drawContours(img, c, -1, (0, 255, 0), 1)
+
+    return img
+
+if __name__ == '__main__':
+    img = cv2.imread('input-images/slides/slide1.jpg')
+    # img = cv2.imread('input-images/training/square.jpg')
+
+    img = cv2.resize(img, (0,0), fx=0.2, fy=0.2)
+    thresh = threshold(img)
+    img = process(thresh, img)
+
+    cv2.imwrite('result.jpg', img)
+
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
