792e2a894d8f2151dbf90b5e4051a634ac1c8d4c
Jeff Wheeler <jeffwheeler@gmail.com>
2014-02-23 01:24:40 -0800

    Handle merging shapes (I hope? I think...)

--- shape.py
+++ shape.py
@@ -1,27 +1,49 @@
+def sorted_segments(s1, s2):
+    (s1, s2) = (tuple(sorted(s1)), tuple(sorted(s2)))
+    return tuple(sorted((s1, s2)))
+
 # [(v1, (s1, s2)), (v2, (s3, s4))]
 class Shape:
     def __init__(self, s1, s2, v):
-        (s1, s2) = sorted((sorted(s1), sorted(s2)))
-        self.c = [(v, sorted((s1, s2)))]
+        self.c = [(v, sorted_segments(s1, s2))]
 
     def has_vertex(self, vertex):
         return vertex in [v for v, _ in self.c]
 
     def has_segment(self, seg):
-        seg = sorted(seg)
+        seg = tuple(sorted(seg))
         for v, ss in self.c:
             if seg in ss:
                 return True
         return False
 
     def add(self, s1, s2, v):
-        (s1, s2) = sorted((sorted(s1), sorted(s2)))
-        if s1 in self.c[-1][1] or s2 in self.c[-1][1]:
-            self.c.append((v, sorted((s1, s2))))
-        elif s1 in self.c[0][1] or s2 in self.c[0][1]:
-            self.c.insert(0, (v, sorted((s1, s2))))
+        assert not self.is_complete()
+
+        if self.goes_to_head(s1, s2, v):
+            self.c.append((v, sorted_segments(s1, s2)))
+        elif self.goes_to_tail(s1, s2, v):
+            self.c.insert(0, (v, sorted_segments(s1, s2)))
         else:
             print 'Failed to add'
+
+    def goes_to_head(self, s1, s2, v):
+        assert not self.is_complete()
+
+        (s1, s2) = sorted_segments(s1, s2)
+        return s1 in self.c[-1][1] or s2 in self.c[-1][1]
+
+    def goes_to_tail(self, s1, s2, v):
+        assert not self.is_complete()
+
+        (s1, s2) = sorted_segments(s1, s2)
+        return s1 in self.c[0][1] or s2 in self.c[0][1]
+
+    def can_add(self, s1, s2, v):
+        if self.has_vertex(v):
+            return False
+        else:
+            return self.has_segment(s1) or self.has_segment(s2)
 
     def is_complete(self):
         (a, b) = (self.c[0][1][0], self.c[-1][1][0])
@@ -32,6 +54,14 @@ class Shape:
         # them).
         s = set(map(tuple, [a, b, c, d]))
         return len(self.c) > 2 and len(s) == 3
+
+    def area(self):
+        if self.is_complete():
+            # Form vectors a, b, c, etc. such that a+b+c+...=0
+            return 0
+        else:
+            print 'Called area() on incomplete shape'
+            return -1
 
     def get_vertices(self):
         return [v for v, _ in self.c]
@@ -56,22 +86,47 @@ class ShapeList:
         self.shapes = []
 
     def add(self, s1, s2, v):
-        found_home = False
+        found_home = None
 
         for shape in self.shapes:
             if shape.has_vertex(v):
-                found_home = True
-            else:
-                has_s1 = shape.has_segment(s1)
-                has_s2 = shape.has_segment(s2)
-                if has_s1 or has_s2:
-                    assert not found_home, 'Should be merging shapes now'
+                # Already added this vertex
+                found_home = shape
+            elif shape.can_add(s1, s2, v):
+                if found_home:
+                    # We need to merge the shape we just found with the one
+                    # that we had found previously (stored in 'found_home')
+                    self.merge_shapes(found_home, shape)
+                else:
                     shape.add(s1, s2, v)
-                    found_home = True
+                    found_home = shape
 
         if not found_home:
             s = Shape(s1, s2, v)
             self.shapes.append(s)
+
+    def merge_shapes(self, shape1, shape2):
+        print 'Merging shapes'
+
+        assert not shape1.is_complete()
+        assert not shape2.is_complete()
+
+        sh2_head_v, sh2_head_segs = shape2.c[0]
+        sh2_tail_v, sh2_tail_segs = shape2.c[-1]
+
+        if shape1.can_add(sh2_head_segs[0], sh2_head_segs[1], sh2_head_v):
+            # Add starting at the head
+            for sh2_v, (sh2_seg1, sh2_seg2) in shape2:
+                shape1.add(sh2_seg1, sh2_seg2, sh2_v)
+        elif shape1.can_add(sh2_tail_segs[0], sh2_tail_segs[1], sh2_tail_v):
+            # Add starting at tail
+            for sh2_v, (sh2_seg1, sh2_seg2) in reversed(shape2):
+                shape1.add(sh2_seg1, sh2_seg2, sh2_v)
+        else:
+            assert False, 'Couldn\'t merge shapes'
+
+        # Find shape2, delete it from the list of shapes
+        self.shapes.remove(shape2)
 
     def delete_shape_with_vertex(self, v):
         for i, shape in enumerate(self.shapes):
@@ -104,7 +159,7 @@ if __name__ == '__main__':
     sl = ShapeList()
     sl.add(*c1)
     sl.add(*c1)
-    sl.add(*c2)
     sl.add(*c3)
     sl.add(*c4)
+    sl.add(*c2)
     print sl, '\n'
